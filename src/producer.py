4270cac744c96ac5203623e31b22a7eff78a04e9
Pieter Polmans <trianoware@gmail.com>
2019-03-19 19:10:31 +0100

    producer.py now only sends a single record with one tag and one field

--- src/producer.py
+++ src/producer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaProducer
 from time import sleep
 from recordbuilder import RecordBuilder
-
+from temp import temp
 
 if __name__ == '__main__':
     # Constante waardes
@@ -14,39 +14,17 @@ if __name__ == '__main__':
     producer = KafkaProducer(bootstrap_servers=PLATFORM_URL)
 
     while True:
-        # Maak een RecordBuilder aan en voeg InfluxDB tags en fields
-        # toe met add_tag en add_field.
-        alambiek1_record_builder = RecordBuilder()
-        alambiek1_record_builder.add_tag('name', 'alambiek1') \
-            .add_field('temperatuurSensor1', 50) \
-            .add_field('temperatuurSensor2', 60) \
-            .add_field('drukSensor1', 1.3) \
-            .add_field('laagWaterNiveau', False)
+			temperatuur_sensor_1_grote_alambiek = temp.measure()
+			
+			print(temperatuur_sensor_1_grote_alambiek)
 
-        # Eens dat alle fields en tags ingevuld zijn, doe
-        # get_record. Dit returned de record als een JSON string
-        # die op zo een manier gestructureerd is dat chiel en ik
-        # hem heel gemakkelijk verder kunnen gebruiken.
-        alambiek1_record = alambiek1_record_builder.get_record()
+			grote_alambiek_record_builder = RecordBuilder()
+			grote_alambiek_record_builder.add_tag('name', 'groteAlambiek') \
+				.add_field('temperatuurSensor1', temperatuur_sensor_1_grote_alambiek))
 
-        # Hetzelfde maar dan voor de andere alambiek.
-        # We hebben het graag als de meetwaardes van de twee
-        # alambieken uiteengehouden worden door een InfluxDB tag.
-        alambiek2_record_builder = RecordBuilder()
-        alambiek2_record_builder.add_tag('name', 'alambiek2') \
-            .add_field('temperatuurSensor1', 50) \
-            .add_field('temperatuurSensor2', 60) \
-            .add_field('drukSensor1', 1.3) \
-            .add_field('laagWaterNiveau', False)
-        alambiek2_record_builder = RecordBuilder()
-
-        alambiek2_record = alambiek2_record_builder.get_record()
-
-        # Verstuur de records naar onze apache kafka
-        # Vanuit hier zorgen Chiel en ik ervoor dat de data in
-        # een InfluxDB en Grafana dashboard terecht komt.
-        encoding = 'utf-8'
-        producer.send(KAFKA_TOPIC, alambiek1_record.encode(encoding))
-        producer.send(KAFKA_TOPIC, alambiek2_record.encode(encoding))
-        print("test")
-        sleep(THREAD_SLEEP_DURATION)
+			grote_alambiek_record = grote_alambiek_record_builder.get_record()
+				
+			encoding = 'utf-8'
+			producer.send(KAFKA_TOPIC, grote_alambiek_record.encode(encoding))
+			
+			sleep(THREAD_SLEEP_DURATION)
